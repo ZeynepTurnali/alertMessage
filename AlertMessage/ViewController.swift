45324d6808938e8921134374b79042432d70fbd8
zeynepturnali <zeynepturnali23@gmail.com>
2020-09-14 13:40:32 +0300

    first commit

--- AlertMessage/ViewController.swift
+++ AlertMessage/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var userNameText = ""
+    var passwordText = ""
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+    @IBOutlet var passwordAgain: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func signUpButton(_ sender: Any) {
+        
+        if (userName.text == userNameText){
+           makeAlert(alertTitle: "Error!", alertMessage: "Please Enter a UserName")
+        }else if(password.text == passwordText){
+            makeAlert(alertTitle: "Error!", alertMessage: "Please Enter a Password")
+        }else if (password.text != passwordAgain.text){
+            makeAlert(alertTitle: "Error!", alertMessage: "First and Second Passwords Must be Equal :)")
+        }else{
+            makeAlert(alertTitle: "Success", alertMessage: ":D")
+        }
+    }
+    
+    func makeAlert(alertTitle: String, alertMessage: String){
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertController.Style.alert)
+                       
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) {
+                           (UIAlertAction) in print("OK button clicked")
+            }
+                       
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+    }
+        
 }
 
